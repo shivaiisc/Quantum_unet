69b972e47f876ad6be2066c9ccf931b3fbbd07fa
shivaiisc <shivachandhraa@gmail.com>
2024-07-24 22:13:12 +0530

    no comment

--- runq.sh
+++ runq.sh
@@ -1,3 +1,3 @@
-python train.py -ic 3 -p 0 -exp yolo-v8q -o 1 -yp ../yolo/runs/detect/yolov8n/weights/best.pt -q 1
-python train.py -ic 3 -p 0 -exp yolo-v5q -o 1 -yp ../yolo/runs/detect/yolov5l6u/weights/best.pt -q 1
-python train.py -ic 3 -p 0 -exp yolo-v10q -o 1 -yp ../yolo/runs/detect/yolov10/weights/best.pt -q 1
+python train.py -ic 3 -p 0 -exp yolo-v8q -o 1 -yp ../yolo/runs/detect/yolov8n/weights/best.pt -q 1 --unet_path ../Quantum/ckpts/q_unet-0.0/0/best_q_unet.pth
+python train.py -ic 3 -p 0 -exp yolo-v5q -o 1 -yp ../yolo/runs/detect/yolov5l6u/weights/best.pt -q 1 --unet_path ../Quantum/ckpts/q_unet-0.0/0/best_q_unet.pth
+python train.py -ic 3 -p 0 -exp yolo-v10q -o 1 -yp ../yolo/runs/detect/yolov10/weights/best.pt -q 1 --unet_path ../Quantum/ckpts/q_unet-0.0/0/best_q_unet.pth
