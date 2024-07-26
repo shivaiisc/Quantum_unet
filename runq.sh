97d6a4d1ed95ae06f0e98fb86c3653b5168d55d9
shivaiisc <shivachandhraa@gmail.com>
2024-07-27 03:52:28 +0530

    no comment

--- runq.sh
+++ runq.sh
@@ -1,6 +1,6 @@
-python train.py -ic 3 -p 0 -exp q_unet-0.0 --noise 0.0 -o 1 -dev cuda:0 -bs 16 -m q_unet -fs 1
-python train.py -ic 3 -p 0 -exp q_unet-0.1 --noise 0.1 -o 1 -dev cuda:0 -bs 16 -m q_unet
-python train.py -ic 3 -p 0 -exp q_unet-0.2 --noise 0.2 -o 1 -dev cuda:0 -bs 16 -m q_unet
-python train.py -ic 3 -p 0 -exp q_unet-0.3 --noise 0.3 -o 1 -dev cuda:0 -bs 16 -m q_unet
-python train.py -ic 3 -p 0 -exp q_unet-0.4 --noise 0.4 -o 1 -dev cuda:0 -bs 16 -m q_unet
-python train.py -ic 3 -p 0 -exp q_unet-0.5 --noise 0.5 -o 1 -dev cuda:0 -bs 16 -m q_unet
+python train.py -ic 3 -p 0 -exp q_unet_noise/q_unet-0.0 --noise 0.0 -o 1 -dev cuda:0 -bs 16 -m q_unet
+python train.py -ic 3 -p 0 -exp q_unet_noise/q_unet-0.1 --noise 0.1 -o 1 -dev cuda:0 -bs 16 -m q_unet
+python train.py -ic 3 -p 0 -exp q_unet_noise/q_unet-0.2 --noise 0.2 -o 1 -dev cuda:0 -bs 16 -m q_unet
+python train.py -ic 3 -p 0 -exp q_unet_noise/q_unet-0.3 --noise 0.3 -o 1 -dev cuda:0 -bs 16 -m q_unet
+python train.py -ic 3 -p 0 -exp q_unet_noise/q_unet-0.4 --noise 0.4 -o 1 -dev cuda:0 -bs 16 -m q_unet
+python train.py -ic 3 -p 0 -exp q_unet_noise/q_unet-0.5 --noise 0.5 -o 1 -dev cuda:0 -bs 16 -m q_unet
